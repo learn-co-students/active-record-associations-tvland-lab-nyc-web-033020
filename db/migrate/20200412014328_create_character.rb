5b79c7a0239f0cb7385ae90c0f481823c94620ba
mishyjari <mishyjari@outlook.com>
2020-04-11 22:17:02 -0400

    Done.

---
+++ db/migrate/20200412014328_create_character.rb
@@ -0,0 +1,7 @@
+class CreateCharacter < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+    end
+  end
+end
---
+++ db/migrate/20200412015434_add_foreign_ids_to_character.rb
@@ -0,0 +1,6 @@
+class AddForeignIdsToCharacter < ActiveRecord::Migration[6.0]
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
+  end
+end
