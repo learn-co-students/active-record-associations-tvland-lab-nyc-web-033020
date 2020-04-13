f8f317af77983cf928d08629c117cefb1e1c8e9b
github username <github email address>
2020-04-13 03:17:10 -0400

    Done.

---
+++ db/migrate/008_change_shows.rb
@@ -0,0 +1,12 @@
+class ChangeShows << ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string
+  end
+
+end
+
+
+# cant get final migration right
